2e662e431933d403b5ddb764a69a772417c7d81b
kknari <nari907@duksung.ac.kr>
2024-06-01 01:07:44 +0900

    user model 변경

--- server/models.py
+++ server/models.py
@@ -5,8 +5,7 @@ from django.db import models
 class User(models.Model):
     username = models.CharField(max_length=150, unique=True)
     password = models.CharField(max_length=128)
-    first_name = models.CharField(max_length=30, blank=True)
-    last_name = models.CharField(max_length=150, blank=True)
+    nickname = models.CharField(max_length=10, blank=True)
 
     def __str__(self):
         return self.username
