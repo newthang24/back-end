d8d18f4e33ed9edb3c6ead62bf4694b26a2d578c
kknari <nari907@duksung.ac.kr>
2024-06-02 05:02:10 +0900

    login, logout, signup, user_delete, fbv 형식으로 완료

--- server/urls.py
+++ server/urls.py
@@ -1,10 +1,9 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
-from rest_framework import urls
-from rest_framework.authtoken.views import obtain_auth_token
 
-urlpatterns =[
-    path('signup/', views.UserCreate.as_view()),
-    path('api-auth/', include('rest_framework.urls')),
-    path('login/', obtain_auth_token, name='api_token_auth'),
+urlpatterns = [
+    path('signup/', views.user_create, name='user_create'),
+    path('login/', views.user_login, name='user_login'),
+    path('logout/', views.user_logout, name='user_logout'),
+    path('delete/', views.user_delete, name='user_delete'),
 ]
