3846ec46df2f7890f6e7134138f65c94e7f76454
kknari <nari907@duksung.ac.kr>
2024-06-15 22:47:18 +0900

    user 연결 완료

--- server/models.py
+++ server/models.py
@@ -3,62 +3,52 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, Permis
 
 
 class UserManager(BaseUserManager):
-    # 일반 user 생성
     def create_user(self, username, nickname, password=None):
         if not username:
-            raise ValueError('must have user username')
+            raise ValueError('The Username field must be set')
         if not nickname:
-            raise ValueError('must have user nickname')
+            raise ValueError('The Nickname field must be set')
         user = self.model(
-            username = username,
-            nickname = nickname
+            username=username,
+            nickname=nickname
         )
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    # 관리자 user 생성
     def create_superuser(self, username, nickname, password=None):
         user = self.create_user(
-            username,
-            password = password,
-            nickname = nickname,
+            username=username,
+            nickname=nickname,
+            password=password,
         )
-        user.is_admin = True
         user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
         return user
 
-#PermissionsMixin 추가
 class User(AbstractBaseUser, PermissionsMixin):
     id = models.AutoField(primary_key=True)
-    username = models.CharField(max_length=10, null=False, blank=False, unique=True)
+    username = models.CharField(max_length=10, unique=True)
     nickname = models.CharField(max_length=10, blank=True)
     
-    # User 모델의 필수 field
-    is_active = models.BooleanField(default=True)    
-    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
 
-    # 헬퍼 클래스 사용
     objects = UserManager()
 
-    # 사용자의 username field는 nickname으로 설정
     USERNAME_FIELD = 'username'
-    # 필수로 작성해야하는 field
     REQUIRED_FIELDS = ['nickname']
 
     def __str__(self):
         return self.nickname
+
     def has_perm(self, perm, obj=None):
-        return self.is_admin
+        return self.is_superuser
 
     def has_module_perms(self, app_label):
-        return self.is_admin
-
-    @property
-    def is_staff(self):
-        return self.is_admin
+        return self.is_superuser
 
 class SRI(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -70,9 +60,7 @@ class SRI(models.Model):
 
 class Calendar(models.Model):
 
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    #user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
     # user 부분 주석 풀 때 null=True 임시로 해 주고 데이터 채운 뒤 변경해야 함
 
     year = models.IntegerField()
