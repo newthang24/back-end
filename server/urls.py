d5d2ac37b33b813b49daa0c57876d147e2bf8fa0
kknari <nari907@duksung.ac.kr>
2024-06-16 05:13:32 +0900

    emotion 완성git add .

--- server/urls.py
+++ server/urls.py
@@ -15,6 +15,7 @@ urlpatterns = [
     path('login/', views.user_login, name='user_login'),
     path('logout/', views.user_logout, name='user_logout'),
     path('delete/', views.user_delete, name='user_delete'),
+    
     path('stats/<int:year>/<int:month>/', views.get_monthly_stats, name='get-monthly-stats'),
     path('calendar/<int:year>/<int:month>/', views.get_monthly_calendar, name='get-monthly-calendar'),
 
@@ -23,4 +24,8 @@ urlpatterns = [
     
     # 감정 기록 결과 저장 및 불러오기
     path('emotions/', views.emotion_list_create, name='emotion-list-create'),
+    # 새로운 감정 분석
+    path('analyze-emotion/', views.analyze_emotion, name='analyze-emotion'),
+    # 새로운 감정 기록 저장
+    path('save-emotion/', views.save_emotion, name='save-emotion'),
 ]
